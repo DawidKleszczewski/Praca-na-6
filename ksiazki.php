28808ffec7de7e5d3745a215759cef69ed6e1763
DawidKleszczewski <dawid.kleszczewski9@gmail.com>
2022-07-29 00:23:01 +0200

    xd

--- ksiazki.php
+++ ksiazki.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $conn = mysqli_connect(
     'localhost',
@@ -9,7 +8,3 @@ $conn = mysqli_connect(
 
 header("Location: index.html");
 ?>
-=======
-$conn = mysqli_connect()
-echo xd
->>>>>>> 71b4e2c20488467bf2772eee378a6ab25531098a
